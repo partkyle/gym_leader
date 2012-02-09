b3ba54d6fa5c65edb8561f1b1cb743fbe24af3ec
Kyle Partridge <partkyle@gmail.com>
2012-02-08 22:17:40 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crossfit::Application.config.secret_token = 'bfa162b3661eabb0ab7872867edefa005626c084035fcafdd3d640fdfe136415a9829a31e62a26d2af98d94477e5af5be0e820392631e1f60833d7c79717c48a'
