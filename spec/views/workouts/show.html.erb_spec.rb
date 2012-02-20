7747b31f530a8e5d9767cbbfd123e55d71c2553b
Kyle Partridge <partkyle@gmail.com>
2012-02-19 22:48:16 -0800

    more tests

--- spec/views/workouts/show.html.erb_spec.rb
+++ spec/views/workouts/show.html.erb_spec.rb
@@ -11,8 +11,8 @@ describe "workouts/show" do
     rendered.should have_content(@workout.key)
     rendered.should have_content(@workout.name)
 
-    @workout.description.split.each do |str|
-      rendered.should have_content(str)
+    @workout.description.lines.each do |str|
+      rendered.should have_content(str.strip)
     end
   end
 end
