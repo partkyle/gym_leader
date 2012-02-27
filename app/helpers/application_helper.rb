0625fba1f89b9982776f099d0a8dc84f966a02fa
Kyle Partridge <partkyle@gmail.com>
2012-02-26 19:36:44 -0800

    added a bootstrap helper for alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def flash_class(flash)
+    css = []
+    case flash
+    when :alert
+      css.push "alert-warning"
+    when :error
+      css.push "alert-danger"
+    when :notice
+      css.push "alert-info"
+    when :success
+      css.push "alert-success"
+    else
+      css.push "alert-" + flash.to_s
+    end
+
+    css.join
+  end
 end
