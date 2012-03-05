4f99f83481b96b19c30fb05d8b4f25ce9f71f935
Kyle Partridge <partkyle@gmail.com>
2012-03-04 22:22:12 -0800

    wip mongoid

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,10 @@
-class Workout < ActiveRecord::Base
+class Workout
+  include Mongoid::Document
+
+  field :key, :type => String
+  field :name, :type => String
+  field :description, :type => String
+
   validates :key, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
 end
