a5083a80d4678f5df874b6e17a60fae27bfa0d34
Kyle Partridge <partkyle@gmail.com>
2012-03-04 20:03:32 -0800

    finish rename

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ GymLeader::Application.config.session_store :cookie_store, key: '_gymleader_sess
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Crossfit::Application.config.session_store :active_record_store
+# GymLeader::Application.config.session_store :active_record_store
